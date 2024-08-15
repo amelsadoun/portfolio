5c45196dd4fd4a25fd172a78ca3f50cbd04a5ae0
amelsadoun <amelsadoun.b@gmail.com>
2024-08-15 22:18:50 +0100

    fixed previewimages..again

--- src/components/projects/previewPopup.jsx
+++ src/components/projects/previewPopup.jsx
@@ -1,27 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Popup } from "react-popupify";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
 
 const CustomPopup = ({ info }) => {
-  const [loading, setLoading] = useState(true);
-  const [loadedImages, setLoadedImages] = useState(0);
-
-  const handleImageLoad = () => {
-    setLoadedImages((prevCount) => prevCount + 1);
-  };
-
-  useEffect(() => {
-    if (loadedImages === info.previewImages.length) {
-      setLoading(false); // All images are loaded
-    }
-  }, [loadedImages, info.previewImages.length]);
+  const [loading, setLoading] = useState(false);
 
   return (
     <Popup
       popupId={info.name}
       animation="bounce"
-      open={false}
+      open={false} 
       closeOnEscape={true}
       closeOnOutsideClick={true}
       closeButton={true}
@@ -42,10 +31,9 @@ const CustomPopup = ({ info }) => {
           {info.previewImages.map((image, index) => (
             <div key={index}>
               <img
-                className="max-h-[90vh] max-w-[800px] object-contain"
+                className="max-h-[90vh] max-w-[800px] object-contain "
                 src={image}
                 alt={`Preview ${index + 1}`}
-                onLoad={handleImageLoad} // Track when an image loads
               />
             </div>
           ))}
